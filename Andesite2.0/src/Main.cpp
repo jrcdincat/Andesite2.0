aa96578fce188f6572295def433545f6d1a6bd05
DESKTOP-ADFBECM\jrcdi <jrcdincat@gmail.com>
2021-10-01 16:43:32 -0400

    Remove unused code

--- Andesite2.0/src/Main.cpp
+++ Andesite2.0/src/Main.cpp
@@ -19,37 +19,6 @@ SDL_Window* window;
 SDL_Renderer* renderer;
 b2World* physicsWorld;
 
-// Components
-// ----------------------------
-struct Rigidbody2DComponent
-{
-	enum class BodyType { Static = 0, Dynamic, Kinematic};
-	BodyType Type = BodyType::Static;
-	bool FixedRotation = false;
-	
-	void* RuntimeBody = nullptr;
-
-	Rigidbody2DComponent() = default;
-	Rigidbody2DComponent(const Rigidbody2DComponent&) = default;
-};
-
-struct BoxCollider2DComponent
-{
-	//Vector2D Offset = { 0.0f, 0.0f };
-	//Vector2D Offset = { 0.5f, 0.5f };
-
-	float Density = 1.0f;
-	float Friction = 0.5f;
-	float Restitution = 0.0f;
-	float RestitutionThreshold = 0.5f;
-
-	void* RuntimeFixture = nullptr;
-
-	BoxCollider2DComponent() = default;
-	BoxCollider2DComponent(const BoxCollider2DComponent&) = default;
-};
-
-// ----------------------------
 b2Body* addRect(int x, int y, int w, int h, bool dyn = true)
 {
 	b2BodyDef bodyDef;
