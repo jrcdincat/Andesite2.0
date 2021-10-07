5653246ca59bee85ad3d1d30901e967827a2dc9d
DESKTOP-ADFBECM\jrcdi <jrcdincat@gmail.com>
2021-10-07 00:20:52 -0400

    Add constants header

---
+++ Andesite2.0/src/Constants.h
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace constants {
+	constexpr int SCREEN_WIDTH = 1080;
+	constexpr int SCREEN_HEIGHT = 720;
+	constexpr float JUMP_FORCE = 20.0f;
+	constexpr float JUMP_TIME = 15.0f;
+}
