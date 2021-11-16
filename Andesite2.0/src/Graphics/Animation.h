853de9ac1cef256ef72708348ba9afb58db82c26
DESKTOP-ADFBECM\jrcdi <jrcdincat@gmail.com>
2021-11-16 16:40:20 -0500

    Refactor Animation

--- Andesite2.0/src/Graphics/Animation.h
+++ Andesite2.0/src/Graphics/Animation.h
@@ -1,21 +1,23 @@
 #pragma once
 
 class Animation {
-public:
-	Animation();
-	void Update(); 
-	void Draw(float x, float y, int spriteWidth, int spriteHeight); 
-	void SetProperties(std::string id,bool repeat, int row, int frameCnt, int animateSpeed, SDL_RendererFlip flip = SDL_FLIP_NONE);
-	inline bool IsEnded() { return isEnded; }
-	inline std::string GetTextureID() { return textureID; }
+	public:
+		Animation();
 
-private:
-	int spriteRow;
-	int currentFrame;
-	int frameCount;
-	int animationSpeed;
-	bool isEnded;
-	bool isRepeated;
-	SDL_RendererFlip rendererFlip;
-	std::string textureID;
+		void Update(); 
+		void Draw(float x, float y, int spriteWidth, int spriteHeight); 
+		void SetProperties(std::string id,bool repeat, int row, int frameCnt, int animateSpeed, SDL_RendererFlip flip = SDL_FLIP_NONE);
+		
+		inline bool IsEnded() { return isEnded; }
+		inline std::string GetTextureID() { return textureID; }
+
+	private:
+		int spriteRow;
+		int currentFrame;
+		int frameCount;
+		int animationSpeed;
+		bool isEnded;
+		bool isRepeated;
+		SDL_RendererFlip rendererFlip;
+		std::string textureID;
 };
