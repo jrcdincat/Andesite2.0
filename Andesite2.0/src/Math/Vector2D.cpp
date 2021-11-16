548fe40a4f5aedc7ace68b7132d0b5b47e4b4c06
DESKTOP-ADFBECM\jrcdi <jrcdincat@gmail.com>
2021-11-16 15:11:20 -0500

    Refactor Vector2D

--- Andesite2.0/src/Math/Vector2D.cpp
+++
@@ -1,2 +0,0 @@
-#include "../pch.h"
-#include "Vector2D.h";
--- Andesite2.0/src/Math/Vector2D.h
+++ Andesite2.0/src/Math/Vector2D.h
@@ -1,20 +1,21 @@
 #pragma once
 
 class Vector2D {
-public: 
-	float x, y;
+	public: 
+		float x, y;
 
-	Vector2D()
-		:x(0.0f), y(0.0f)
-	{
-	}
-	Vector2D(float xParam, float yParam)
-		:x(xParam), y(yParam)
-	{
-	}
+		Vector2D()
+			:x(0.0f), y(0.0f)
+		{
+		}
 
-	// Multiply vector by scalar
-	inline Vector2D operator * (const float scalar) const {
-		return Vector2D(x * scalar, y * scalar);
-	}
+		Vector2D(float xParam, float yParam)
+			:x(xParam), y(yParam)
+		{
+		}
+
+		// Multiply vector by scalar
+		inline Vector2D operator * (const float scalar) const {
+			return Vector2D(x * scalar, y * scalar);
+		}
 };
