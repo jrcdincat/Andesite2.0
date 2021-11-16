073f3d0e065153394b8ab034eb6638bb44c215e8
DESKTOP-ADFBECM\jrcdi <jrcdincat@gmail.com>
2021-11-16 14:55:31 -0500

    Refactor actor

--- Andesite2.0/src/Objects/Actor.h
+++ Andesite2.0/src/Objects/Actor.h
@@ -2,19 +2,14 @@
 #include "GameObject.h"
 
 class Actor : public GameObject {
+	public: 
+		Actor(Properties* properties) : GameObject(properties){}
+		virtual ~Actor() {}
 
-public: 
-	Actor() = default;
-	Actor(Properties* properties) : GameObject(properties){}
-	virtual ~Actor() {}
+		virtual void Draw() = 0;
+		virtual void Update(float dt) = 0;
+		virtual void Clean() = 0;
 
-	virtual void Draw() = 0;
-	virtual void Update(float dt) = 0;
-	virtual void Clean() = 0;
-
-	virtual void UpdateAnimationState() = 0;
-	virtual int GetCurrentState() = 0;
-
-protected: 
-	std::string name; 
+		virtual void UpdateAnimationState() = 0;
+		virtual int GetCurrentState() = 0;
 };
