71ad97092db8ccc0e795d373b7d57875b9d179ec
DESKTOP-ADFBECM\jrcdi <jrcdincat@gmail.com>
2021-11-16 16:19:54 -0500

    Refactor Command

--- Andesite2.0/src/Input/Command.h
+++ Andesite2.0/src/Input/Command.h
@@ -2,42 +2,36 @@
 #include "../Objects/Player.h"
 
 class Command {
-public: 
-	virtual void execute(Player* actor) = 0;
+	public: 
+		virtual void execute(Player* actor) = 0;
 };
 
 class JumpCommand : public Command
 {
-public:
-	virtual void execute(Player* actor);
+	public:
+		virtual void execute(Player* actor);
 };
 
 class JumpLeftCommand : public Command
 {
-public:
-	virtual void execute(Player* actor);
+	public:
+		virtual void execute(Player* actor);
 };
 
 class JumpRightCommand : public Command
 {
-public:
-	virtual void execute(Player* actor);
+	public:
+		virtual void execute(Player* actor);
 };
 
 class MoveLeftCommand : public Command
 {
-public:
-	virtual void execute(Player* actor);
+	public:
+		virtual void execute(Player* actor);
 };
 
 class MoveRightCommand : public Command
 {
-public:
-	virtual void execute(Player* actor);
-};
-
-class EscapeCommand : public Command
-{
-public: 
-	virtual void execute(Player* actor);
+	public:
+		virtual void execute(Player* actor);
 };
