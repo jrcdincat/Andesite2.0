8193334aaae99536f38fe1d7ae1cead8c3d9b7ad
DESKTOP-ADFBECM\jrcdi <jrcdincat@gmail.com>
2021-11-16 14:18:01 -0500

    Refactor IObject

--- Andesite2.0/src/Objects/IObject.h
+++ Andesite2.0/src/Objects/IObject.h
@@ -1,10 +1,9 @@
 #pragma once
-#include "Entity.h"
+
 class IObject
 {
-public:
-	virtual void Draw() = 0;
-	virtual void Update(float dt) = 0;
-	virtual void Clean() = 0; 
+	public:
+		virtual void Draw() = 0;
+		virtual void Update(float dt) = 0;
+		virtual void Clean() = 0; 
 };
-
